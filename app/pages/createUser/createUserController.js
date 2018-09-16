56ae5c41df1c1445263992257fcc7fa5e583a99e
Richard Virag <andraderv10@gmail.com>
2018-09-16 15:10:17 -0300

    Trazer json de usuários para tabela usando http

--- app/pages/createUser/createUserController.js
+++ app/pages/createUser/createUserController.js
@@ -1,41 +1,19 @@
 (function () {
     "use strict";
     angular.module('bomControle')
-        .controller('createUserController', ['$scope', function ($scope, userHttpServices) {
-            $scope.usuarios = [
-                {
-                    nome: "Pedro Henrique",
-                    email: "pedro_henrique_01@gmail.com",
-                    cpf: "85907965070",
-                    dataNascimento: "1957-10-16",
-                    sexo: "M",
-                    telefone: "1112345678",
-                    celular: "11963289652",
-                    cep: "49082490",
-                    endereco: "Rua Sargento Lauro José",
-                    bairro: "Novo Paraíso",
-                    cidade: "Aracaju",
-                    estado: "SE"
-                },
-                {
-                    nome: "Maria Clara",
-                    email: "maria.clara@hotmail.com",
-                    cpf: "32883173036",
-                    dataNascimento: "1986-04-21",
-                    sexo: "F",
-                    telefone: "1332147896",
-                    celular: "13974185296",
-                    cep: "59614840",
-                    endereco: "Rua Nossa Senhora de Lourdes",
-                    bairro: "Santa Delmira",
-                    cidade: "Mossoró",
-                    estado:  "RN"
-                },
-            ];
+        .controller('createUserController', ['$scope', 'userHttpServices', function ($scope, userHttpServices) {
+
+            userHttpServices.getUsers()
+            .then(function (response) {
+                $scope.usuarios = response.data;
+                $.each($scope.usuarios, function(i){
+                    $scope.usuarios[i].phone = $scope.usuarios[i].phone.replace(/\s+/g, '');
+                });
+            });
 
             $scope.delete = function (usuario) {
                 $.each($scope.usuarios, function(i){
-                    if($scope.usuarios[i].nome === usuario.nome) {
+                    if($scope.usuarios[i].name === usuario.name) {
                         $scope.usuarios.splice(i,1);
                         return false;
                     }
@@ -43,7 +21,7 @@
       			};
 
             $scope.add = function (usuario) {
-        				$scope.usuarios.push(angular.copy(usuario));
+        				$scope.usuarios.push(angular.copy(usuario))
         				delete $scope.usuario;
         				$scope.userRegister.$setPristine();
       			};
