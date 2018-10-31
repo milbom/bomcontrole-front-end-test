d3b64b7de03d5e9e70e621b391e43434179b40ff
Leandro Queiroz <ls.queiroz@outlook.com>
2018-10-30 21:34:12 -0300

    adding the sass compiler

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var sourcemaps = require('gulp-sourcemaps');
 var concatCss = require('gulp-concat-css');
 var cssmin = require('gulp-cssmin');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+sass.compiler = require('node-sass');
 var paths = {
   appDependencesCss: [
     './node_modules/animate.css/animate.css/',
@@ -31,6 +33,9 @@ var paths = {
     './node_modules/angular-input-masks/releases/angular-input-masks-standalone.js',
     './node_modules/angular-ui-notification/dist/angular-ui-notification.js',
   ],
+  appSass: [
+    './app/**/**/*.scss',
+  ],
   appJs: [
     './app/app.js',
     './app/**/**/**/*.js',
@@ -38,27 +43,14 @@ var paths = {
   appHtml: './app/**/**/**/**/*.html',
 };
 
-$.gulp.task('build-css', ['concat-dependences-css'], function () {
-  return $.gulp.src('./public/css/*.css')
-    .pipe(cssmin())
-    .pipe($.gulp.dest('./public/css'));
-});
-
-$.gulp.task('build-js', ['concat-dependences-js', 'concat-app-js'], function () {
-  return $.gulp.src('./public/js/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe($.gulp.dest('./public/js'));
-});
-
+// Dependences
 $.gulp.task('concat-dependences-css', function () {
   return $.gulp.src(paths.appDependencesCss)
     .pipe(concatCss('dependences.css'))
+    .pipe(cssmin())
     .pipe($.gulp.dest('./public/css'))
     .pipe($.browserSync.stream());
 });
-
 
 $.gulp.task('concat-dependences-js', function () {
   return $.gulp.src(paths.appDependencesJs)
@@ -76,20 +68,51 @@ $.gulp.task('concat-app-js', function () {
     .pipe($.gulp.dest('./public/js'));
 });
 
+// Builds
+// Sass
+$.gulp.task('build:sass', function () {
+  return $.gulp.src(paths.appSass)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concatCss('sass.css'))
+    .pipe(cssmin())
+    .pipe($.gulp.dest('./public/css/'));
+});
+
+// Css
+$.gulp.task('build:css', ['concat-dependences-css'], function () {
+  return $.gulp.src('./public/css/*.css')
+    .pipe(concatCss('dependences.css'))
+    .pipe(cssmin())
+    .pipe($.gulp.dest('./public/css'));
+});
+
+// Js
+$.gulp.task('build:js', ['concat-dependences-js', 'concat-app-js'], function () {
+  return $.gulp.src('./public/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe($.gulp.dest('./public/js'));
+});
+
+// Browser reload
 $.gulp.task('browser-sync-reload', function (done) {
   $.browserSync.reload();
   done();
 });
 
+// Serve
 $.gulp.task('serve', [
-  'build-js',
-  'build-css',
+  'build:js',
+  'build:css',
+  'build:sass',
 ], function () {
 
-  $.gulp.watch(paths.appJs, ['build-js']);
+  $.gulp.watch(paths.appJs, ['build:js']);
+  $.gulp.watch(paths.appSass, ['build:sass']);
   $.gulp.watch('./public/js/app.js', ['browser-sync-reload']);
   $.gulp.watch('./app/**/**/**/*.css', ['browser-sync-reload']);
-
+  $.gulp.watch('./app/**/**/**/*.scss', ['browser-sync-reload']);
   $.gulp.watch(paths.appHtml, ['browser-sync-reload']);
 
   return $.browserSync.init({
